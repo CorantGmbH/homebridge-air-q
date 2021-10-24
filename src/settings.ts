c3a013d515d2f4314a40a79eb4eace805b50537a
dl2080 <daniel.lehmann@runbox.com>
2021-10-24 14:53:47 +0200

    Changed platfrom back to ExampleHomebridgePlugin for testing

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'air-Q';
+export const PLATFORM_NAME = 'ExampleHomebridgePlugin';
 
 /**
  * This must match the name of your plugin as defined the package.json
