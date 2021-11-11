5159f16a4a5ba54a4a383843b6498bb9e64b899c
dl2080 <daniel.lehmann@runbox.com>
2021-11-11 18:53:19 +0100

    Module for http request to air-Q which returns retrieved data as JSON

---
+++ src/httpRequest.ts
@@ -0,0 +1,27 @@
+import { request } from 'http';
+import { decrypt } from './decryptAES256';
+
+export function performRequest(options,airqpass) {
+  return new Promise<string>((resolve, reject) => {
+    request(
+      options,
+      function(response) {
+        const { statusCode } = response;
+        if (statusCode && statusCode >= 300) {
+          reject(
+            new Error(response.statusMessage)
+          )
+        }
+        const chunks: any[] = [];
+        response.on('data', (chunk) => {
+          chunks.push(chunk);
+        });
+        response.on('end', () => {
+          const result = Buffer.concat(chunks).toString();
+          resolve(decrypt(JSON.parse(result).content, airqpass));
+        });
+      }
+    )
+      .end();
+  })
+}
